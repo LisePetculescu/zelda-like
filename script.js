a6cfba52fbc0990a71eda3266e25830d8be0c91b
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-11-15 14:42:26 +0100

    collesion detection needs alert of something when true

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 window.addEventListener("load", start);
 
-/* TODO: when player moves shit between pixels of player 
-when moving left switch between walking left oics
-*/
-
-let playerPosition = {
+let player = {
   x: 100,
   y: 100,
-  speed: 50 // px/s
+  speed: 70, // px/s
+  width: 30,
+  height: 40
 };
+
+let enemy = {
+  x: 200,
+  y: 200,
+  speed: 0,
+  width: 30,
+  height: 40
+};
+
 const gameField = {
   width: 400,
   height: 400
 };
-const playerSize = {
-  width: 60, // Set the width of the player
-  height: 80 // Set the height of the player
-};
+
 let controls = {
   up: false,
   down: false,
@@ -24,14 +28,11 @@ let controls = {
   right: false
 };
 
-let lastTime = 0;
-// let direction = 0;
-let direction = { x: 0, y: 0 };
-
 function start() {
   console.log("start.. ");
 
-  displayPlayer();
+  // displayPlayer();
+
   requestAnimationFrame(tick);
 
   document.addEventListener("keydown", (event) => {
@@ -45,109 +46,7 @@ function start() {
   });
 }
 
-function displayPlayer() {
-  const visualPlayer = document.querySelector("#player");
-
-  //  look Left
-  // visualPlayer.style.backgroundPosition = "200% 200%"
-  // walk left
-  // visualPlayer.style.backgroundPosition = "300% 200%"
-
-  // look right
-  //    visualPlayer.style.backgroundPosition = "200% 100%";
-  // walk right
-  //    visualPlayer.style.backgroundPosition = "300% 100%";
-
-  // look down
-  // visualPlayer.style.backgroundPosition = "400% 400%";
-
-  // walk down
-  // visualPlayer.style.backgroundPosition = "300% 400%";
-
-  // look up
-  // visualPlayer.style.backgroundPosition = "400% 300%";
-  // walk up
-  // visualPlayer.style.backgroundPosition = "300% 300%";
-
-  visualPlayer.style.translate = `${playerPosition.x}px ${playerPosition.y}px`;
-
-  console.log("player coordinates: ", playerPosition);
-}
-
-function tick(time) {
-  requestAnimationFrame(tick);
-
-  const deltaTime = (time - lastTime) / 1000;
-  lastTime = time;
-
-  updateDirection(deltaTime);
-
-  // Check for collision with game field boundaries using the size of the player
-  if (playerPosition.x < -2) {
-    playerPosition.x = 0;
-  }
-  if (playerPosition.x + playerSize.width / 2 > gameField.width + 2) {
-    playerPosition.x = gameField.width - playerSize.width / 2;
-  }
-  if (playerPosition.y < -2) {
-    playerPosition.y = 0;
-  }
-  if (playerPosition.y + playerSize.height / 2 > gameField.height + 2) {
-    playerPosition.y = gameField.height - playerSize.height / 2;
-  }
-
-  displayPlayer();
-}
-
-function movingDirection(event) {
-  switch (event.key) {
-    case "w":
-    case "ArrowUp":
-      direction.y = -1;
-      break;
-    case "s":
-    case "ArrowDown":
-      direction.y = 1;
-      break;
-    case "a":
-    case "ArrowLeft":
-      direction.x = -1;
-      break;
-    case "d":
-    case "ArrowRight":
-      direction.x = 1;
-      break;
-  }
-}
-
-// function movePlayer(deltaTime) {
-//   const movement = deltaTime / playerPosition.speed;
-
-//   switch (direction) {
-//     case 0:
-//       playerPosition.y = playerPosition.y - movement;
-//       break;
-//     case 1:
-//       playerPosition.y = playerPosition.y + movement;
-//       break;
-//     case 2:
-//       playerPosition.x = playerPosition.x - movement;
-//       break;
-//     case 3:
-//       playerPosition.x = playerPosition.x + movement;
-//       break;
-//   }
-
-//   //   playerPosition.x = playerPosition.x + 10;
-
-//   displayPlayer();
-//   //   playerPosition.speed = playerPosition.speed + 10;
-// }
-
-function canMove(player, position) {
-  if (playerPosition.x > 0 || playerPosition.x < gameField.width || playerPosition.y > 0 || playerPosition.y < gameField.height) return false;
-  else return true;
-}
+// *************** controls *******************
 
 function updateKeyState(key, isPressed) {
   switch (key) {
@@ -170,20 +69,116 @@ function updateKeyState(key, isPressed) {
   }
 }
 
+// ************* view **********************
+
+function displayPlayer() {
+  const visualPlayer = document.querySelector("#player");
+
+  visualPlayer.style.translate = `${player.x}px ${player.y}px`;
+}
+
+function displayEnemy() {
+  const visualEnemy = document.querySelector("#enemy");
+  visualEnemy.style.translate = `${enemy.x}px ${enemy.y}px`;
+}
+
+// ******** tick / view *********
+
+let lastTime = 0;
+function tick(time) {
+  requestAnimationFrame(tick);
+
+  const deltaTime = (time - lastTime) / 1000;
+  lastTime = time;
+
+  movePlayer(deltaTime);
+  // moveEnemy(deltaTime);
+
+  // check collisions
+  if (isColliding(player, enemy)) {
+    player.collision = true;
+    enemy.collision = true;
+  } else {
+    player.collision = false;
+    enemy.collision = false;
+  }
+
+  // update display
+  displayPlayer();
+  displayEnemy();
+  // displayCollisionInfo();
+}
+
 // lav check om midlertidig position er lovlig
-function updateDirection(deltaTime) {
-  if (canMove) {
-    if (controls.up) {
-      playerPosition.y -= playerPosition.speed * deltaTime;
-    }
-    if (controls.down) {
-      playerPosition.y += playerPosition.speed * deltaTime;
-    }
-    if (controls.left) {
-      playerPosition.x -= playerPosition.speed * deltaTime;
-    }
-    if (controls.right) {
-      playerPosition.x += playerPosition.speed * deltaTime;
-    }
+function movePlayer(deltaTime) {
+  const position = { x: player.x, y: player.y };
+  let deltaY = 0;
+  let deltaX = 0;
+
+  if (controls.up) {
+    deltaY -= player.speed * deltaTime;
+  } else if (controls.down) {
+    deltaY += player.speed * deltaTime;
+  }
+  if (controls.left) {
+    deltaX -= player.speed * deltaTime;
+  } else if (controls.right) {
+    deltaX += player.speed * deltaTime;
+  }
+
+  if (deltaX != 0 && deltaY != 0) {
+    // console.log("player bevæger sig diagonalt");
+
+    const dist = Math.hypot(deltaX, deltaY);
+    // const dist = 1.4142;
+
+    // console.log("dist hpot: ", dist);
+
+    deltaX = deltaX / dist;
+    deltaY = deltaY / dist;
+  }
+
+  position.x += deltaX;
+  position.y += deltaY;
+
+  if (canMove(player, position)) {
+    player.x = position.x;
+    player.y = position.y;
   }
 }
+
+// function moveEnemy(deltaTime) {}
+
+function canMove(player, position) {
+  if (position.x < -2 || position.y < -2 || position.x > gameField.width - player.width || position.y > gameField.height - player.height) return false;
+
+  return true;
+}
+
+function isColliding(player, enemy) {
+  // check if bottom of player hits top of enemy
+  const belowTop = player.y + player.h >= enemy.y;
+
+  // check if top of player hits bottom of enemy
+  const aboveBottom = player.y <= enemy.y + enemy.h;
+
+  // check if player's right side hits enemy's left side |P|-->|E|
+  const afterLeft = player.x + player.w >= enemy.x;
+
+  // check if player's left side hits enemy's right side |E|-->|P|
+  const beforeRight = player.x <= enemy.x + enemy.w;
+
+  // Only if all four conditions are met, are the player and enemy colliding
+  return belowTop && aboveBottom && afterLeft && beforeRight;
+}
+
+// function displayCollisionInfo() {
+//   const collisionInfo = document.querySelector("#collision-status");
+//   if (player.collision) {
+//     collisionInfo.textContent = "Collision!!";
+//     collisionInfo.classList.add("collision");
+//   } else {
+//     collisionInfo.textContent = "No collision ...";
+//     collisionInfo.classList.remove("collision");
+//   }
+// }
